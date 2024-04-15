28cfb0cb672df0927083936d440f55a6b15f995b
akash-vericap <akash@vericap.io>
2024-04-15 19:43:20 +0530

    Initialisation: Defined licensing

---
+++ contracts/PCCManager/FungiblePCC.sol
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2024 Vericap Pte. Ltd.
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+/**
+ * @author Vericap Blockchain Engineering Team
+ * @notice A PCC token represents a future carbon credits 
+ */
+
+contract PlannedCarbonCredit {
+    
+}
