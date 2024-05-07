07ffc99054fd811be5efbc46e4ddd1c1841e0442
akash-vericap <akash@vericap.io>
2024-05-07 13:02:02 +0530

    Deployment Scripts: Fixed contract dependencies

--- scripts/deploy-pcc-factory.js
+++ scripts/deploy-pcc-factory.js
@@ -3,48 +3,50 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require('hardhat')
-require('dotenv').config()
+const hre = require("hardhat");
+require("dotenv").config();
 
 async function main() {
-	// Hardhat always runs the compile task when running scripts with its command
-	// line interface.
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
 
-	// If this script is run directly using `node` you may want to call compile
-	// manually to make sure everything is compiled
-	let PCCFactory
-	let pccFactory
-	try {
-		await hre.run('compile')
-		PCCFactory = await hre.ethers.getContractFactory('PCCFactory')
-		pccFactory = await upgrades.deployProxy(
-			PCCFactory,
-			[process.env.ADMIN_WALLET_ADDRESS],
-			{ kind: 'uups' }
-		)
-		await pccFactory.deployed()
-	} catch (err) {
-		console.log('Contract deployment failed', err)
-	}
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  let PlannedCreditFactory;
+  let plannedCreditFactory;
+  try {
+    await hre.run("compile");
+    PlannedCreditFactory = await hre.ethers.getContractFactory(
+      "PlannedCreditFactory"
+    );
+    plannedCreditFactory = await upgrades.deployProxy(
+      PlannedCreditFactory,
+      [process.env.ADMIN_WALLET_ADDRESS],
+      { kind: "uups" }
+    );
+    await plannedCreditFactory.deployed();
+  } catch (err) {
+    console.log("Contract deployment failed", err);
+  }
 
-	const waitForDeployment = (seconds) => {
-		console.log(
-			`\n\x1b[33m${'[waiting]'}\x1b[0m Preparing PCCFactory smart contract deployment. Just a moment... \n`
-		)
-		setTimeout(() => {
-			console.log(
-				`\x1b[1m\x1b[32m${'[success]'}\x1b[0m PCCFactory smart contrat deployed successfully to: \x1b[4mhttps://mumbai.polygonscan.com/address/${
-					pccFactory.address
-				}#code\x1b[0m \n`
-			)
-		}, seconds)
-	}
-	waitForDeployment(3000)
+  const waitForDeployment = (seconds) => {
+    console.log(
+      `\n\x1b[33m${"[waiting]"}\x1b[0m Preparing PCCFactory smart contract deployment. Just a moment... \n`
+    );
+    setTimeout(() => {
+      console.log(
+        `\x1b[1m\x1b[32m${"[success]"}\x1b[0m PCCFactory smart contrat deployed successfully to: \x1b[4mhttps://sepolia.etherscan.io/address/${
+          plannedCreditFactory.address
+        }#code\x1b[0m \n`
+      );
+    }, seconds);
+  };
+  waitForDeployment(3000);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-	console.error(error)
-	process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
--- scripts/deploy-pcc-manager.js
+++ scripts/deploy-pcc-manager.js
@@ -3,51 +3,53 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require('hardhat')
-require('dotenv').config()
+const hre = require("hardhat");
+require("dotenv").config();
 
 async function main() {
-	// Hardhat always runs the compile task when running scripts with its command
-	// line interface.
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
 
-	// If this script is run directly using `node` you may want to call compile
-	// manually to make sure everything is compiled
-	let PCCManager
-	let pccManager
-	try {
-		await hre.run('compile')
-		PCCManager = await hre.ethers.getContractFactory('PCCManager')
-		pccManager = await upgrades.deployProxy(
-			PCCManager,
-			[
-				process.env.ADMIN_WALLET_ADDRESS,
-				process.env.PCC_FACTORY_CONTRACT_ADDRESS,
-			],
-			{ kind: 'uups' }
-		)
-		await pccManager.deployed()
-	} catch (err) {
-		console.log('Contract deployment failed', err)
-	}
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  let PlannedCreditManager;
+  let plannedCreditManager;
+  try {
+    await hre.run("compile");
+    PlannedCreditManager = await hre.ethers.getContractFactory(
+      "PlannedCreditManager"
+    );
+    plannedCreditManager = await upgrades.deployProxy(
+      PlannedCreditManager,
+      [
+        process.env.ADMIN_WALLET_ADDRESS,
+        process.env.PLANNED_CREDIT_FACTORY_CONTRACT_ADDRESS,
+      ],
+      { kind: "uups" }
+    );
+    await plannedCreditManager.deployed();
+  } catch (err) {
+    console.log("Contract deployment failed", err);
+  }
 
-	const waitForDeployment = (seconds) => {
-		console.log(
-			`\n\x1b[33m${'[waiting]'}\x1b[0m Preparing PCCManager smart contract deployment. Just a moment... \n`
-		)
-		setTimeout(() => {
-			console.log(
-				`\x1b[1m\x1b[32m${'[success]'}\x1b[0m PCCManager smart contrat deployed successfully to: \x1b[4mhttps://mumbai.polygonscan.com/address/${
-					pccManager.address
-				}#code\x1b[0m \n`
-			)
-		}, seconds)
-	}
-	waitForDeployment(3000)
+  const waitForDeployment = (seconds) => {
+    console.log(
+      `\n\x1b[33m${"[waiting]"}\x1b[0m Preparing Planned Credit Manager smart contract deployment. Just a moment... \n`
+    );
+    setTimeout(() => {
+      console.log(
+        `\x1b[1m\x1b[32m${"[success]"}\x1b[0m Planned Credit Manager smart contrat deployed successfully to: \x1b[4mhttps://sepolia.etherscan.io/address/${
+          plannedCreditManager.address
+        }#code\x1b[0m \n`
+      );
+    }, seconds);
+  };
+  waitForDeployment(3000);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-	console.error(error)
-	process.exitCode = 1
-})
+  console.error(error);
+  process.exitCode = 1;
+});
